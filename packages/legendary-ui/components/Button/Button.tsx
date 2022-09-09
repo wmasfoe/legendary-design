4fa509b106e37344576b00d357f06dc760039318
Jiaqi <lijq1103@gmail.com>
2022-09-10 05:11:04 +0800

    fix: 修复 classname 覆盖问题

--- packages/legendary-ui/components/Button/Button.tsx
+++ packages/legendary-ui/components/Button/Button.tsx
@@ -1,23 +1,24 @@
 import { memo } from "react"
 import type { ButtonProps } from './types'
 import { useClassNames, useDefaultProps } from '@legendary/hooks'
+import './style.scss'
 
 const Button = (props: ButtonProps) => {
   const { btnType, children, disabled, ...other } = useDefaultProps(props, {
     btnType: 'primary',
     disabled: false
   })
-  const classnames = useClassNames(['btn', `btn-${btnType}`], {
+  const classnames = useClassNames(['btn', `btn-${btnType}`, props.className], {
     disabled,
   })
 
-  return <>
+  return <div className="l-button">
     {
       btnType === 'link' ?
-        <a className={classnames} {...other}>{children}</a>:
-        <button className={classnames} disabled={disabled} {...other}>{ children }</button>
+        <a {...other} className={classnames}>{children}</a>:
+        <button {...other} className={classnames} disabled={disabled}>{ children }</button>
     }
-  </>;
+  </div>;
 };
 
 export default memo(Button);
