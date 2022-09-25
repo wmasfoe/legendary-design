01cb6365f0b5d4766e366f2ec2ddf09f7a470dd9
Jiaqi <lijq1103@gmail.com>
2022-09-25 18:21:15 +0800

    feat: 新建组件script

---
+++ packages/legendary-ui/components/Card/Card.tsx
@@ -0,0 +1,8 @@
+import { memo } from 'react';
+
+
+const Card = () => {
+  return <></>;
+}
+
+export default memo(Card);
---
+++ scripts/template/__name__/__name__.tsx
@@ -0,0 +1,7 @@
+import { memo } from 'react';
+
+const __name__ = () => {
+  return <></>;
+}
+
+export default memo(__name__);
