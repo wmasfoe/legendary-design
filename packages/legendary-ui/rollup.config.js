25cbc1df82c854fdd10074b600576059c85da268
Jiaqi <lijq1103@gmail.com>
2023-05-08 19:08:57 +0800

    wip: 打包脚本

---
+++ packages/legendary-ui/rollup.config.js
@@ -0,0 +1,57 @@
+import typescript from 'rollup-plugin-typescript2';
+import scss from 'rollup-plugin-scss';
+import clear from 'rollup-plugin-clear';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import { uglify } from 'rollup-plugin-uglify';
+import copy from 'rollup-plugin-copy';
+
+export default {
+  input: ['./components/index.ts'],
+  output: [
+    {
+      file: 'web-react/cjs/index.js',
+      format: 'cjs',
+      name: 'cjs.js',
+    },
+    {
+      file: 'web-react/umd/index.js',
+      format: 'umd',
+      name: 'umd.js',
+    },
+    {
+      file: 'web-react/es/index.js',
+      format: 'es',
+      name: 'index.js',
+    },
+  ],
+  plugins: [
+    typescript(), // 会自动读取 文件tsconfig.json配置
+    scss({ output: './web-react/style/index.css' }),
+    clear({
+      targets: ['web-react'],
+    }),
+    resolve(),
+    commonjs({
+      ignoreGlobal: true,
+      include: /\/node_modules\//,
+      external: ['react', 'react-dom', 'styled-components'],
+      namedExports: {
+        react: Object.keys(require('react')),
+        'react-is': Object.keys(require('react-is')),
+      },
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      runtimeHelpers: true,
+    }),
+    terser(),
+    uglify(),
+    copy({
+      targets: [{ src: './styles/index.scss', dest: 'web-react/style' }],
+    }),
+  ],
+  external: ['react', 'react-dom'],
+};
